82e9a44bcc2b27281c83c8b0748383fe56d69691
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2016-10-05 21:23:28 +0100

    bug fix

--- acf/Kepler_ACF.py
+++ acf/Kepler_ACF.py
@@ -230,7 +230,7 @@ def corr_run(time, flux, flux_err, id, savedir=None, saveplot=False):
     t.add_column('harmonic_det', harmonic_det)
     t.add_column('amp_all', amp_all)
     t.add_column('amp_per', amp_per)
-    return period, dlag_per_err
+    return float(period), float(dlag_per_err)
 
 def acf_calc(time, flux, interval, kid, max_psearch_len):
 
