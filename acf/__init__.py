4061382633d0016ca2c829c33c927efbced6e950
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2016-10-05 20:25:23 +0100

    bug fixes

--- acf/__init__.py
+++ acf/__init__.py
@@ -2,4 +2,9 @@
 # -*- coding: utf-8 -*-
 
 from .simple_acf import simple_acf
-from Kepler_ACF import corr_run
+from .Kepler_ACF import corr_run
+from .KOI_tools_b12 import *
+from .filter import *
+from .gls import *
+from .mpfit import *
+from .tools import *
